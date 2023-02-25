9185e2c36e9f8ba66f98aab10ad5186b1f838983
Peter Osoo <posoo617@gmail.com>
2023-02-25 13:58:32 +0300

    display books

--- src/components/books.js
+++ src/components/books.js
@@ -1,20 +1,38 @@
+import img1 from "../images/book-1.webp"
+import img2 from "../images/book-2.webp"
+import img3 from "../images/book-3.jpg"
+import img4 from "../images/book-4.jpg"
+import img5 from "../images/book-5.jpg"
+
 export const books = [
 	{
 		id: 1,
 		title: "Rachael's Good Eats",
-		img: "https://m.media-amazon.com/images/I/812hbyotzEL._AC_UL600_FMwebp_QL65_.jpg",
+		img: img1,
 		author: "Rachael DeVaux",
 	},
 	{
 		id: 2,
 		title: "The Game Changing Attorney",
-		img: "https://m.media-amazon.com/images/I/51HwGoi+nIL._AC_UL600_FMwebp_QL65_.jpg",
+		img: img2,
 		author: "Michael Mogill",
 	},
 	{
 		id: 3,
 		title: "What My Bones Know",
-		img: "https://m.media-amazon.com/images/I/51ZBaKBB3ML.jpg",
+		img: img3,
 		author: "Stephanie Foo",
+	},
+	{
+		id: 4,
+		title: "Days On The Road",
+		img: img4,
+		author: "Sarah Raymond Herndon",
+	},
+	{
+		id: 5,
+		title: "The Midnight Library",
+		img: img5,
+		author: "Matt Haig",
 	},
 ]
