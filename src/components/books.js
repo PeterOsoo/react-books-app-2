90a1b672cff91b7dacc5f8780e55d76333c51a20
Peter Osoo <posoo617@gmail.com>
2023-02-25 13:24:56 +0300

    books

---
+++ src/components/books.js
@@ -0,0 +1,20 @@
+export const books = [
+	{
+		id: 1,
+		title: "Rachael's Good Eats",
+		img: "https://m.media-amazon.com/images/I/812hbyotzEL._AC_UL600_FMwebp_QL65_.jpg",
+		author: "Rachael DeVaux",
+	},
+	{
+		id: 2,
+		title: "The Game Changing Attorney",
+		img: "https://m.media-amazon.com/images/I/51HwGoi+nIL._AC_UL600_FMwebp_QL65_.jpg",
+		author: "Michael Mogill",
+	},
+	{
+		id: 3,
+		title: "What My Bones Know",
+		img: "https://m.media-amazon.com/images/I/51ZBaKBB3ML.jpg",
+		author: "Stephanie Foo",
+	},
+]
