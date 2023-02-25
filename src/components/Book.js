0a2c6280b5aadceeb55ab2e9a6590fe4c06f9d60
Peter Osoo <posoo617@gmail.com>
2023-02-25 13:25:04 +0300

    display books

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,14 +1,13 @@
 import React from "react"
 
-const Book = () => {
+const Book = props => {
+	const { img, title, author } = props
+
 	return (
 		<article className="book">
-			<img
-				src="https://m.media-amazon.com/images/I/51HwGoi+nIL._AC_UL600_FMwebp_QL65_.jpg"
-				alt="The Game Changing Attorney"
-			/>
-			<h2>The Game Changing Attorney</h2>
-			<h4>Michael Mogill</h4>
+			<img src={img} alt={title} />
+			<h2>{title} </h2>
+			<h4>{author} </h4>
 		</article>
 	)
 }
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,17 +1,18 @@
 import React from "react"
 import Book from "./Book"
 
+import { books } from "./books"
+
 const BookList = () => {
 	return (
-		<section className="booklist">
-			{" "}
-			<Book />
-			<Book />
-			<Book />
-			<Book />
-			<Book />
-			<Book />
-		</section>
+		<div className="title">
+			<h1>Best Selling Books</h1>
+			<section className="booklist">
+				{books.map((book, index) => {
+					return <Book {...book} key={book.id} number={index} />
+				})}
+			</section>
+		</div>
 	)
 }
 
